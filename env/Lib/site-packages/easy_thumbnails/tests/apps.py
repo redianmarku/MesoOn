d2247434959c832c877433461c73b1b13f35f001
Redi <redian1marku@gmail.com>
2020-04-17 23:26:32 +0200

    ewfew

--- env/Lib/site-packages/easy_thumbnails/tests/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EasyThumbnailsTestConfig(AppConfig):
-    name = 'easy_thumbnails.tests'
-    label = 'easy_thumbnails_tests'
--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\redia\Desktopi\Udemy-Clone-Django\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
